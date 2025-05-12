52bbd8c685db9b64a8e50470a1fea84d87cd4df6
paulichdom <paulic.domagoj@gmail.com>
2025-05-12 07:42:52 +0200

    implement a getter method

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -1,11 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
 public class QuestionSo : ScriptableObject
 {
      [TextArea(2,6)]
-     [SerializeField] string _question = "Enter new question text here";
+     [SerializeField] string question = "Enter new question text here";
+
+     public string GetQuestion()
+     {
+          return question;
+     }
     
 }
