09e2359fb09a3d5666fb97c0f0e72e1c7894d24f
paulichdom <paulic.domagoj@gmail.com>
2025-05-12 07:58:08 +0200

    serialize answers array and correct answer index

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -5,10 +5,22 @@ public class QuestionSo : ScriptableObject
 {
      [TextArea(2,6)]
      [SerializeField] string question = "Enter new question text here";
+     [SerializeField] string[] answers = new string[4];
+     [SerializeField] private int correctAnswerIndex;
 
      public string GetQuestion()
      {
           return question;
+     }
+
+     public int GetCorrectAnswerIndex()
+     {
+          return correctAnswerIndex;
+     }
+
+     public string GetAnswer(int index)
+     {
+          return answers[index];
      }
     
 }
